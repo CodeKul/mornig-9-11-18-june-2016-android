891a04cc11c65243f735837cad476466914ef6c7
aniruddha <aniruddha.kudalkar@gmail.com>
2016-06-30 10:57:19 +0530

    initial commit

--- app/src/main/java/com/codekul/interactivitycommunication/MainActivity.java
+++ app/src/main/java/com/codekul/interactivitycommunication/MainActivity.java
@@ -5,17 +5,98 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_IMG_ID = "imageId";
+    private static final Integer REQUEST_PROFILE_ACT = 4563;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button btnLogin =
-                (Button) findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new Click());
+        initButtons();
+
+        changeImageSrc(R.drawable.analytics);
+    }
+
+    private void initButtons(){
+
+        final Click click = new Click();
+
+        final Button btnProfile =
+                (Button) findViewById(R.id.btnProfile);
+        btnProfile.setOnClickListener(click);
+
+        final Button btnChange =
+                (Button) findViewById(R.id.btnChange);
+        btnChange.setOnClickListener(click);
+    }
+
+    private void changeImageSrc(int id){
+
+        final ImageView imageView =
+                (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(id);
+    }
+
+    private void changeImageSrc(){
+
+        int []imgs = {
+                R.drawable.analytics,
+                R.drawable.circle,
+                R.drawable.cpu,
+                R.drawable.hands,
+                R.drawable.my
+        };
+
+        int index = (int)(Math.random() * 5);
+
+        final ImageView imageView =
+                (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(imgs[index]);
+        imageView.setTag(imgs[index]);
+    }
+
+    private int getImageSrc(){
+
+        final ImageView imageView =
+                (ImageView) findViewById(R.id.imageView);
+        return (int) imageView.getTag();
+    }
+
+    private void fireNewActivityIntent(){
+
+        Intent intent =
+                new Intent(/*context*/MainActivity.this,
+                                /*typeInformation of target and not the bytecode file*/ProfileActiviity.class);
+
+        Bundle bundle =
+                new Bundle();
+        bundle.putInt(KEY_IMG_ID,getImageSrc());
+
+        intent.putExtras(bundle);
+
+        //startActivity(intent);
+
+        startActivityForResult(intent,REQUEST_PROFILE_ACT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_PROFILE_ACT){
+
+            if(resultCode == RESULT_OK){
+
+                Bundle bundle = data.getExtras();
+                int imageId = bundle.getInt(KEY_IMG_ID);
+                changeImageSrc(imageId);
+            }
+        }
     }
 
     private class Click implements View.OnClickListener {
@@ -23,14 +104,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            if(v.getId() == R.id.btnLogin){
-
-                Intent intent =
-                        new Intent(/*context*/MainActivity.this,
-                                /*typeInformation of target and not the bytecode file*/ProfileActiviity.class);
-
-                startActivity(intent);
-
+            if(v.getId() == R.id.btnProfile){
+                fireNewActivityIntent();
+            }
+            else {
+              changeImageSrc();
             }
         }
     }
--- app/src/main/java/com/codekul/interactivitycommunication/ProfileActiviity.java
+++ app/src/main/java/com/codekul/interactivitycommunication/ProfileActiviity.java
@@ -1,7 +1,11 @@
 package com.codekul.interactivitycommunication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfileActiviity extends AppCompatActivity {
 
@@ -9,5 +13,88 @@ public class ProfileActiviity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_activiity);
+
+        loadBundleData();
+
+        initButtons();
+    }
+
+
+    private void changeImageSrc(int id){
+
+        final ImageView imageView =
+                (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(id);
+    }
+
+    private void loadBundleData(){
+
+        Intent responsibleIntent =
+                getIntent();
+
+        Bundle bundle =
+                responsibleIntent.getExtras();
+        if(bundle != null){
+
+            int imageId = bundle.getInt(MainActivity.KEY_IMG_ID);
+            changeImageSrc(imageId);
+        }
+    }
+
+    private void changeImageSrc(){
+
+        int []imgs = {
+                R.drawable.analytics,
+                R.drawable.circle,
+                R.drawable.cpu,
+                R.drawable.hands,
+                R.drawable.my
+        };
+
+        int index = (int)(Math.random() * 5);
+
+        final ImageView imageView =
+                (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(imgs[index]);
+        imageView.setTag(imgs[index]);
+    }
+
+    private int getImageSrc(){
+
+        final ImageView imageView =
+                (ImageView) findViewById(R.id.imageView);
+        return (int) imageView.getTag();
+    }
+
+    private void initButtons(){
+
+        final Button btnChange = (Button) findViewById(R.id.btnChange);
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                changeImageSrc();
+            }
+        });
+
+        final Button btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentData =
+                        new Intent();
+
+                Bundle bundle =
+                        new Bundle();
+                bundle.putInt(MainActivity.KEY_IMG_ID,getImageSrc());
+
+                intentData.putExtras(bundle);
+
+                setResult(RESULT_OK,intentData);
+
+                finish();
+            }
+        });
     }
 }
